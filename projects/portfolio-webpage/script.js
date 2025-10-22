f4bb5e0ca3b08e68c8fa3d29fdd7d1f6bcb93686
sagar <sagar.y.praveen@gmail.com>
2025-10-22 11:02:25 +0530

    Initial commit with project files

---
+++ projects/portfolio-webpage/script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+const name = document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const message = document.getElementById("message").value.trim();
+
+     if (name && email && message) {
+     alert(`Thank you, ${name}! Your message has been sent.`);
+    this.reset();
+  } else {
+    alert("Please fill in all fields before submitting.");
+  }
+});
